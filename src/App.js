e663eba16a803a24b4098ee5f780d7a9aa01ce9c
sanyukta001 <sanyuktamishra911@gmail.com>
2025-02-05 13:19:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import react from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import react from 'react'
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+// import { components } from 'react';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <nav>
+        <ul>
+          <li>
+          <Link to="/">Login</Link>
+          </li>
+          <li>
+          <Link to="/signup">Sign Up</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route exact="true" path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+      </div>
+      <div className='centered-div'>
+        <h1>Stay Protected Online : Detect</h1>
+        <p>Our Advanced AI- Powwered Solution Identifies malicious websites. safeguarding, your data and privacy.</p>
+        <div className='detect'>
+          <input className='url-holder' type="text" placeholder="Enter a URL" />
+          <button className='detectButton'>Detect</button>
+        </div>
+      </div>
+      </Router>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        //Login authentication logic
+    };
+
+    return(
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <input
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+                <input
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+export default Login;
